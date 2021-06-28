c04105e1cc4f061653a312b671369e67cf0cb92e
sinxno <smith.jahnsen@gmail.com>
2021-06-28 13:34:15 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace SiemensToIgnitionTagConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTIATagList = new System.Windows.Forms.Label();
+            this.textBoxTIATagList = new System.Windows.Forms.TextBox();
+            this.buttonTIATagList = new System.Windows.Forms.Button();
+            this.labelIgnitionFolderName = new System.Windows.Forms.Label();
+            this.labelIgnitionOPCConnectionName = new System.Windows.Forms.Label();
+            this.buttonSaveIgnitionTagList = new System.Windows.Forms.Button();
+            this.textBoxIgnitionFolderName = new System.Windows.Forms.TextBox();
+            this.textBoxIgnitionOPCConnectionName = new System.Windows.Forms.TextBox();
+            this.openFileDialogTIATagList = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialogSaveIgnitionTagList = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // labelTIATagList
+            // 
+            this.labelTIATagList.AutoSize = true;
+            this.labelTIATagList.Location = new System.Drawing.Point(13, 13);
+            this.labelTIATagList.Name = "labelTIATagList";
+            this.labelTIATagList.Size = new System.Drawing.Size(84, 17);
+            this.labelTIATagList.TabIndex = 0;
+            this.labelTIATagList.Text = "TIA Tag List";
+            // 
+            // textBoxTIATagList
+            // 
+            this.textBoxTIATagList.Location = new System.Drawing.Point(224, 10);
+            this.textBoxTIATagList.Name = "textBoxTIATagList";
+            this.textBoxTIATagList.Size = new System.Drawing.Size(411, 22);
+            this.textBoxTIATagList.TabIndex = 1;
+            // 
+            // buttonTIATagList
+            // 
+            this.buttonTIATagList.Location = new System.Drawing.Point(642, 7);
+            this.buttonTIATagList.Name = "buttonTIATagList";
+            this.buttonTIATagList.Size = new System.Drawing.Size(146, 29);
+            this.buttonTIATagList.TabIndex = 2;
+            this.buttonTIATagList.Text = "Load Tag List";
+            this.buttonTIATagList.UseVisualStyleBackColor = true;
+            this.buttonTIATagList.Click += new System.EventHandler(this.buttonTIATagList_Click);
+            // 
+            // labelIgnitionFolderName
+            // 
+            this.labelIgnitionFolderName.AutoSize = true;
+            this.labelIgnitionFolderName.Location = new System.Drawing.Point(13, 62);
+            this.labelIgnitionFolderName.Name = "labelIgnitionFolderName";
+            this.labelIgnitionFolderName.Size = new System.Drawing.Size(138, 17);
+            this.labelIgnitionFolderName.TabIndex = 3;
+            this.labelIgnitionFolderName.Text = "Ignition Folder Name";
+            // 
+            // labelIgnitionOPCConnectionName
+            // 
+            this.labelIgnitionOPCConnectionName.AutoSize = true;
+            this.labelIgnitionOPCConnectionName.Location = new System.Drawing.Point(13, 111);
+            this.labelIgnitionOPCConnectionName.Name = "labelIgnitionOPCConnectionName";
+            this.labelIgnitionOPCConnectionName.Size = new System.Drawing.Size(202, 17);
+            this.labelIgnitionOPCConnectionName.TabIndex = 4;
+            this.labelIgnitionOPCConnectionName.Text = "Ignition OPC Connection Name";
+            // 
+            // buttonSaveIgnitionTagList
+            // 
+            this.buttonSaveIgnitionTagList.Location = new System.Drawing.Point(13, 160);
+            this.buttonSaveIgnitionTagList.Name = "buttonSaveIgnitionTagList";
+            this.buttonSaveIgnitionTagList.Size = new System.Drawing.Size(185, 32);
+            this.buttonSaveIgnitionTagList.TabIndex = 5;
+            this.buttonSaveIgnitionTagList.Text = "Save Ignition Tag List";
+            this.buttonSaveIgnitionTagList.UseVisualStyleBackColor = true;
+            this.buttonSaveIgnitionTagList.Click += new System.EventHandler(this.buttonSaveIgnitionTagList_Click);
+            // 
+            // textBoxIgnitionFolderName
+            // 
+            this.textBoxIgnitionFolderName.Location = new System.Drawing.Point(224, 59);
+            this.textBoxIgnitionFolderName.Name = "textBoxIgnitionFolderName";
+            this.textBoxIgnitionFolderName.Size = new System.Drawing.Size(411, 22);
+            this.textBoxIgnitionFolderName.TabIndex = 6;
+            // 
+            // textBoxIgnitionOPCConnectionName
+            // 
+            this.textBoxIgnitionOPCConnectionName.Location = new System.Drawing.Point(224, 108);
+            this.textBoxIgnitionOPCConnectionName.Name = "textBoxIgnitionOPCConnectionName";
+            this.textBoxIgnitionOPCConnectionName.Size = new System.Drawing.Size(411, 22);
+            this.textBoxIgnitionOPCConnectionName.TabIndex = 7;
+            // 
+            // openFileDialogTIATagList
+            // 
+            this.openFileDialogTIATagList.FileName = "openFileDialog1";
+            this.openFileDialogTIATagList.Filter = "CSV file|*.csv";
+            this.openFileDialogTIATagList.Title = "Open TIA Tag List";
+            // 
+            // saveFileDialogSaveIgnitionTagList
+            // 
+            this.saveFileDialogSaveIgnitionTagList.DefaultExt = "json";
+            this.saveFileDialogSaveIgnitionTagList.FileName = "IgnitionTagList.json";
+            this.saveFileDialogSaveIgnitionTagList.Filter = "JSON File | *.json";
+            this.saveFileDialogSaveIgnitionTagList.Title = "Save Ignition Tag List";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxIgnitionOPCConnectionName);
+            this.Controls.Add(this.textBoxIgnitionFolderName);
+            this.Controls.Add(this.buttonSaveIgnitionTagList);
+            this.Controls.Add(this.labelIgnitionOPCConnectionName);
+            this.Controls.Add(this.labelIgnitionFolderName);
+            this.Controls.Add(this.buttonTIATagList);
+            this.Controls.Add(this.textBoxTIATagList);
+            this.Controls.Add(this.labelTIATagList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTIATagList;
+        private System.Windows.Forms.TextBox textBoxTIATagList;
+        private System.Windows.Forms.Button buttonTIATagList;
+        private System.Windows.Forms.Label labelIgnitionFolderName;
+        private System.Windows.Forms.Label labelIgnitionOPCConnectionName;
+        private System.Windows.Forms.Button buttonSaveIgnitionTagList;
+        private System.Windows.Forms.TextBox textBoxIgnitionFolderName;
+        private System.Windows.Forms.TextBox textBoxIgnitionOPCConnectionName;
+        private System.Windows.Forms.OpenFileDialog openFileDialogTIATagList;
+        private System.Windows.Forms.SaveFileDialog saveFileDialogSaveIgnitionTagList;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SiemensToIgnitionTagConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonTIATagList_Click(object sender, EventArgs e)
+        {
+            if (openFileDialogTIATagList.ShowDialog() == DialogResult.OK)
+            {
+                textBoxTIATagList.Text = openFileDialogTIATagList.FileName;
+            }
+        }
+
+        private void buttonSaveIgnitionTagList_Click(object sender, EventArgs e)
+        {
+            if (textBoxTIATagList.TextLength<1)
+            {
+                MessageBox.Show("TIA Tag List can not be empty");
+            }
+            else if (textBoxIgnitionFolderName.TextLength < 1)
+            {
+                MessageBox.Show("Ignition Folder Name can not be empty");
+            }
+            else if (textBoxIgnitionOPCConnectionName.TextLength < 1)
+            {
+                MessageBox.Show("Ignition OPC Connection Name can not be empty");
+            }
+            else if (saveFileDialogSaveIgnitionTagList.ShowDialog() == DialogResult.OK)
+            {
+                BuildJSONFile(saveFileDialogSaveIgnitionTagList.FileName);
+            }
+        }
+
+        private void BuildJSONFile(string savePath)
+        {
+            string[] lines = System.IO.File.ReadAllLines(textBoxTIATagList.Text);
+            string last = lines.Last();
+            string first = lines.First();
+
+            using (StreamWriter writer = new StreamWriter(savePath))
+            {
+                writer.WriteLine("{");
+                writer.WriteLine("  \"name\": \"" + textBoxIgnitionFolderName.Text + "\",");
+                writer.WriteLine("  \"tagType\": \"Folder\",");
+                writer.WriteLine("  \"tags\": [");
+                //TODO: Get all tags from csv and convert them to JSON format
+
+
+                foreach(string line in lines)
+                {
+                    if (line.Equals(first))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        string[] columns = line.Split(';');
+                        writer.WriteLine("    {");
+                        writer.WriteLine("      \"valueSource\": \"opc\",");
+                        string adressString = columns[3].Remove(0,1);
+                        string nameString = columns[0];
+
+                        //searching for some illegal characters
+                        while (nameString.Contains('.'))
+                        {
+                           nameString = nameString.Replace('.', '_');
+                            //int place = nameString.IndexOf('.')
+                            //nameString.Remove(place, 1).Insert(place, '_');
+                        }
+
+                        while (nameString.Contains('/'))
+                        {
+                            nameString = nameString.Replace('/', '_');
+                        }
+                        
+                        if (adressString.Contains('.'))
+                        {
+                            //bit access is being used. Ignition needs an X in the adressing.
+                            string numbers = string.Empty;
+                            string letters = string.Empty;
+                            for (int i = 0; i < adressString.Length; i++)
+                            {
+                                if (Char.IsDigit(adressString[i]) || Char.Equals(adressString[i], '.'))
+                                {
+                                    numbers += adressString[i];
+                                }
+                                else
+                                {
+                                    letters += adressString[i];
+                                }
+                            }
+                            letters += 'X';
+
+                            adressString = letters + numbers;
+                        }
+
+                        writer.WriteLine("      \"opcItemPath\": \"[" + textBoxIgnitionOPCConnectionName.Text + "]" + adressString + "\",");
+
+                        switch (columns[2])
+                        {
+                            case "Word":
+                                
+                                writer.WriteLine("      \"dataType\": \"" + "Int4" + "\",");//TODO correlate TIA datatype with Ignition Datatype
+                                break;
+
+                            case "Real":
+                                writer.WriteLine("      \"dataType\": \"" + "Float4" + "\",");//TODO correlate TIA datatype with Ignition Datatype
+                                break;
+
+                            case "Int":
+                                writer.WriteLine("      \"dataType\": \"" + "Int4" + "\",");//TODO correlate TIA datatype with Ignition Datatype
+                                break;
+
+                            case "Bool":
+                                writer.WriteLine("      \"dataType\": \"" + "Boolean" + "\",");//TODO correlate TIA datatype with Ignition Datatype
+                                break;
+
+                            case "Time":
+                                writer.WriteLine("      \"dataType\": \"" + "DateTime" + "\",");//TODO correlate TIA datatype with Ignition Datatype
+                                break;
+
+                            case "Byte":
+                                writer.WriteLine("      \"dataType\": \"" + "Int1" + "\",");//TODO correlate TIA datatype with Ignition Datatype
+                                break;
+
+
+                            default:
+                                writer.WriteLine("      \"dataType\": \"" + columns[2] + "\",");//TODO correlate TIA datatype with Ignition Datatype
+                                break;
+                        }
+
+                        
+                        writer.WriteLine("      \"name\": \"" + nameString + "\",");
+                        writer.WriteLine("      \"tagType\": \"AtomicTag\",");
+                        writer.WriteLine("      \"opcServer\": \"Ignition OPC UA Server\"");
+                        if (line.Equals(last))
+                        {
+                            writer.WriteLine("    }");
+                        }
+                        else
+                        {
+                            writer.WriteLine("    },");
+                        }
+                    }
+                }
+
+
+
+                writer.WriteLine("  ]");
+                writer.WriteLine("}");
+            }
+        }
+    }
+}
