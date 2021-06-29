929c05809f9fe0cb5e8987729b067f4d2be6e1ca
sinxno <smith.jahnsen@gmail.com>
2021-06-29 12:19:29 +0200

    Corrected the Data type adressing

--- Form1.cs
+++ Form1.cs
@@ -73,79 +73,112 @@ namespace SiemensToIgnitionTagConverter
                         string[] columns = line.Split(';');
                         writer.WriteLine("    {");
                         writer.WriteLine("      \"valueSource\": \"opc\",");
-                        string adressString = columns[3].Remove(0,1);
+                        string adressString = columns[3].Remove(0,1); //Removes the % from the adress
                         string nameString = columns[0];
-
-                        //searching for some illegal characters
+                        string ignitionJSONNameString = String.Empty;
+                        //searching for some illegal characters in the name
                         while (nameString.Contains('.'))
                         {
                            nameString = nameString.Replace('.', '_');
-                            //int place = nameString.IndexOf('.')
-                            //nameString.Remove(place, 1).Insert(place, '_');
                         }
 
                         while (nameString.Contains('/'))
                         {
                             nameString = nameString.Replace('/', '_');
                         }
+
+                        string numbers = string.Empty;
+                        string letters = string.Empty;
+                        for (int i = 0; i < adressString.Length; i++)
+                        {
+                            if (Char.IsDigit(adressString[i]) || Char.Equals(adressString[i], '.'))
+                            {
+                                numbers += adressString[i];
+                            }
+                            else
+                            {
+                                letters += adressString[i];
+                            }
+                        }
+                        if (letters.Length>1)
+                        {
+                            letters = letters.Remove(1); //Only keep the first letter defining if it is an Input, Output, Memory etc..
+                        }
                         
+
                         if (adressString.Contains('.'))
                         {
                             //bit access is being used. Ignition needs an X in the adressing.
-                            string numbers = string.Empty;
-                            string letters = string.Empty;
-                            for (int i = 0; i < adressString.Length; i++)
-                            {
-                                if (Char.IsDigit(adressString[i]) || Char.Equals(adressString[i], '.'))
-                                {
-                                    numbers += adressString[i];
-                                }
-                                else
-                                {
-                                    letters += adressString[i];
-                                }
-                            }
+                            
+                            
                             letters += 'X';
 
-                            adressString = letters + numbers;
                         }
 
-                        writer.WriteLine("      \"opcItemPath\": \"[" + textBoxIgnitionOPCConnectionName.Text + "]" + adressString + "\",");
+                        
 
                         switch (columns[2])
                         {
                             case "Word":
-                                
-                                writer.WriteLine("      \"dataType\": \"" + "Int4" + "\",");//TODO correlate TIA datatype with Ignition Datatype
+                                letters += 'W';
+                                ignitionJSONNameString="      \"dataType\": \"" + "Int4" + "\",";//TODO correlate TIA datatype with Ignition Datatype
                                 break;
 
                             case "Real":
-                                writer.WriteLine("      \"dataType\": \"" + "Float4" + "\",");//TODO correlate TIA datatype with Ignition Datatype
+                                letters += "REAL";
+                                ignitionJSONNameString = "      \"dataType\": \"" + "Float4" + "\",";//TODO correlate TIA datatype with Ignition Datatype
                                 break;
 
                             case "Int":
-                                writer.WriteLine("      \"dataType\": \"" + "Int4" + "\",");//TODO correlate TIA datatype with Ignition Datatype
+                                letters += 'I';
+                                ignitionJSONNameString = "      \"dataType\": \"" + "Int4" + "\",";//TODO correlate TIA datatype with Ignition Datatype
                                 break;
 
                             case "Bool":
-                                writer.WriteLine("      \"dataType\": \"" + "Boolean" + "\",");//TODO correlate TIA datatype with Ignition Datatype
+                                ignitionJSONNameString = "      \"dataType\": \"" + "Boolean" + "\",";//TODO correlate TIA datatype with Ignition Datatype
                                 break;
 
                             case "Time":
-                                writer.WriteLine("      \"dataType\": \"" + "DateTime" + "\",");//TODO correlate TIA datatype with Ignition Datatype
+                                ignitionJSONNameString = "      \"dataType\": \"" + "DateTime" + "\",";//TODO correlate TIA datatype with Ignition Datatype
                                 break;
 
                             case "Byte":
-                                writer.WriteLine("      \"dataType\": \"" + "Int1" + "\",");//TODO correlate TIA datatype with Ignition Datatype
+                                letters += 'B';
+                                ignitionJSONNameString = "      \"dataType\": \"" + "Int1" + "\",";//TODO correlate TIA datatype with Ignition Datatype
+                                break;
+
+                            case "Char":
+                                letters += 'C';
+                                ignitionJSONNameString = "      \"dataType\": \"" + "String" + "\",";//TODO correlate TIA datatype with Ignition Datatype
+                                break;
+
+                            case "DWord":
+                                letters += 'D';
+                                ignitionJSONNameString = "      \"dataType\": \"" + "Int8" + "\",";//TODO correlate TIA datatype with Ignition Datatype
+                                break;
+
+                            case "DInt":
+                                letters += "DI";
+                                ignitionJSONNameString = "      \"dataType\": \"" + "Int8" + "\",";//TODO correlate TIA datatype with Ignition Datatype
+                                break;
+
+                            case "String":
+                                letters += "STRING";
+                                ignitionJSONNameString = "      \"dataType\": \"" + "String" + "\",";//TODO correlate TIA datatype with Ignition Datatype
                                 break;
 
 
                             default:
-                                writer.WriteLine("      \"dataType\": \"" + columns[2] + "\",");//TODO correlate TIA datatype with Ignition Datatype
+                                ignitionJSONNameString = "      \"dataType\": \"" + "Boolean" + "\",";//TODO correlate TIA datatype with Ignition Datatype
                                 break;
                         }
 
-                        
+                        adressString = letters + numbers;
+
+                        writer.WriteLine("      \"opcItemPath\": \"[" + textBoxIgnitionOPCConnectionName.Text + "]" + adressString + "\",");
+                        writer.WriteLine(ignitionJSONNameString);
+
+
                         writer.WriteLine("      \"name\": \"" + nameString + "\",");
                         writer.WriteLine("      \"tagType\": \"AtomicTag\",");
                         writer.WriteLine("      \"opcServer\": \"Ignition OPC UA Server\"");
